8fb7c5c1d1111e08f7e63aab21eb1a2580e66332
mark <mark@tranquillussoftware.co.uk>
2013-12-03 18:14:53 +0000

    Add akka state machine

--- app/controllers/Websocket.scala
+++ app/controllers/Websocket.scala
@@ -4,7 +4,6 @@ import play.api._
 import play.api.mvc._
 import play.api.libs.iteratee.Iteratee
 import play.api.libs.iteratee.Enumerator
-import scala.collection.mutable.Stack
 import play.api.libs.iteratee.Concurrent
 import akka.actor.Actor
 import play.api.libs.concurrent.Akka
@@ -14,37 +13,49 @@ import akka.actor.Props
 import akka.pattern.ask
 import akka.util.Timeout
 import concurrent.duration._
+import concurrent._
 
 object Websocket extends Controller {
 
   val reverser = Akka.system.actorOf(Props[Reverser])
   
-  def index = WebSocket.using[String] { request =>
+  def index = WebSocket.async[String] { request =>
+    future {
+    	val (outenum, outchannel) = Concurrent.broadcast[String]
+    	implicit val timeout = Timeout(5 seconds)
 
-    val (outenum, outchannel) = Concurrent.broadcast[String]
-    implicit val timeout = Timeout(5 seconds)
-    
-    val in = Iteratee.foreach[String]{
-      s => (reverser ? StringMsg(s)).map {
-        	case StringMsg(stringMsg) => outchannel.push(stringMsg)
-      }  			
-      println(s)
-    }.mapDone { _ =>
-      println("Disconnected")
+	    var in = Iteratee.foreach[String]{
+	      s => (reverser ? StringMsg(s)).map {
+	        	case StringMsg(stringMsg) => outchannel.push(stringMsg)
+	      }  			
+	      println(s)
+	    }.mapDone { _ =>
+	      println("Disconnected")
+	    }
+	    (in, outenum)
     }
-
-    (in, outenum)
   }
 
   class Reverser extends Actor {
     
-    def receive = {
+    import context._
+    
+    override def receive = receiveAndReverse
+    
+    def receiveAndReverse : Receive = {
       case StringMsg(s) => {
         println(s"Got msg ${s}")
         sender ! StringMsg(s.reverse)
+        become(beAwkward)
       }
     }
     
+    def beAwkward : Receive = {
+      case StringMsg(s) => {
+        sender ! StringMsg("badger")
+        become(receiveAndReverse)
+      }
+    }
   }
  
   case class StringMsg(str : String)
